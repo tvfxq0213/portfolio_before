600bf4f9d6eef7862a04c9d78397d61e2f36e7f4
Narae <alas0213@naver.com>
2020-07-08 17:06:17 +0900

    스타일 변경

--- client/src/components/views/ContactMePage/ContactMePage.js
+++ client/src/components/views/ContactMePage/ContactMePage.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 function ContactMePage() {
   return (
-    <div>
-      
+    <div class="container">
+      <h2 className="page_title ">
+        Contact Me
+      </h2>
+
+      <div class="info_wrap ">
+        H.P : 0.1.0.5.3.8.1.6.4.5.4<br/>
+        <a href="mailto:jjaing1313@gmail.com">E-mail : jjaing1313@gmail.com</a>
+      </div>
     </div>
   )
 }
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,9 +4,8 @@ import { FaCode } from "react-icons/fa";
 function LandingPage() {
     return (
         <>
-        <div className="app">
-            <FaCode style={{ fontSize: '4rem' }} /><br />
-            <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div class="container">
+         LandingPage
         </div>
         </>
     )
