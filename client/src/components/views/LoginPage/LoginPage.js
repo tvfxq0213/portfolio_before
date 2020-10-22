e46be05d3ccc341baee7db45dd05b9bc48f58939
Narae <alas0213@naver.com>
2020-10-22 15:33:59 +0900

    수정수정

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -75,12 +75,10 @@ function LoginPage(props) {
           values,
           touched,
           errors,
-          dirty,
           isSubmitting,
           handleChange,
           handleBlur,
           handleSubmit,
-          handleReset,
         } = props;
         return (
           <div className="app">
