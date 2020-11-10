0f18da3fecd4b9ddcbb4559074de5a9ca8997f76
Narae <alas0213@naver.com>
2020-11-10 16:33:07 +0900

    HBD Daniel

---
+++ client/src/components/views/HBDKD/LandingPage.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import './../../../style/HBDKD.scss';
+
+
+function HBDLandingPage() {
+    return (
+        <>
+          <section className="section01">
+            <h1 className="title" >
+              HAPPY 25TH BIRTHDAY<br/>
+              KANG DAINEL</h1>
+
+              <div className="cake_wrap">
+
+                <div className="cake">
+                  <div className="plate"></div>
+                  <div className="layer layer-bottom"></div>
+                  <div className="layer layer-middle"></div>
+                  <div className="layer layer-top"></div>
+                  <div className="icing"></div>
+                  <div className="drip drip1"></div>
+                  <div className="drip drip2"></div>
+                  <div className="drip drip3"></div>
+                  <div className="candle candle-long-1 candle-long">
+                    <div className="flame"></div>
+                  </div>
+                  <div className="candle candle-long-2 candle-long">
+                    <div className="flame"></div>
+                  </div>
+                  <div className="candle candle-short-1 candle-short">
+                    <div className="flame"></div>
+                  </div>
+                  <div className="candle candle-short-2 candle-short">
+                    <div className="flame"></div>
+                  </div>
+                  <div className="candle candle-short-3 candle-short">
+                    <div className="flame"></div>
+                  </div>
+                  <div className="candle candle-short-4 candle-short">
+                    <div className="flame"></div>
+                  </div>
+                  <div className="candle candle-short-5 candle-short">
+                    <div className="flame"></div>
+                  </div>
+                </div>
+              </div>
+
+          </section>
+          <section className="msg_list">
+
+
+          </section>
+          <section className="form_wrapper">
+            <form>
+
+            </form>
+          </section>
+
+        </>
+    )
+}
+
+export default HBDLandingPage
